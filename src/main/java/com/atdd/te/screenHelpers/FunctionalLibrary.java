d1d8dc466bf9d2ac5e0ac93ea99c52cb4fb95ec2
- <r17@ms.ds.uhc.com>
2018-08-01 16:53:35 +0530

    updated searchAndEditData fucn

--- src/main/java/com/atdd/te/screenHelpers/FunctionalLibrary.java
+++ src/main/java/com/atdd/te/screenHelpers/FunctionalLibrary.java
@@ -859,7 +859,7 @@ public class FunctionalLibrary extends CommonHelper{
 	{
 	
 	bRes=true;
-	Mainframe_GlobalFunctionLib.sendText(row, 2, "1");
+	Mainframe_GlobalFunctionLib.sendText(row, 2, "2");
 	Mainframe_GlobalFunctionLib.pressKey("Enter");
 	Thread.sleep(2000);
 	}
@@ -879,6 +879,7 @@ public class FunctionalLibrary extends CommonHelper{
 	boolean bRes=false;
 	try{
 	
+		
 	StringTokenizer stData=new StringTokenizer(RowColOfData,",");
 	StringTokenizer stDataSearch=new StringTokenizer(StartRowColToSearch,",");
 	int row=Integer.valueOf(stDataSearch.nextToken());
