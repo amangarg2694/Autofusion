40b61009cf81ef0ec5789a94452909c2d707411b
Priya <spriya7@ms.ds.uhc.com>
2018-08-06 16:31:24 +0530

    pushing the feature files for SR 41427

--- src/test/java/com/atdd/demo/runners/RunDemoTest.java
+++ src/test/java/com/atdd/demo/runners/RunDemoTest.java
@@ -23,7 +23,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(format = { "pretty", "json:target/cucumber.json" },
 
-		features = { "classpath:features/RxClaim/featureFiles/SR41360/SR41360_TC12_TC14.feature" },
+		features = { "classpath:features/RxClaim/featureFiles/SR41427/SR41427_TC04.feature" },
 
 		plugin = { "rerun:target/rerun.txt", "com.cucumber.listener.ExtentCucumberFormatter:", "html:target/cucumber",
 				"json:target/cucumber.json" },
