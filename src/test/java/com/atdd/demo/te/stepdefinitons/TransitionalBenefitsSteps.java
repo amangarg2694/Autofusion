684a662116c8124a65e2296bbfbde47c66cd008b
rmoore65 <rmoore65@LH7U05CG73129VN.ms.ds.uhc.com>
2018-08-31 17:09:03 -0500

    @REggie-Updated tbStepcode so that it will add When statement.

--- src/test/java/com/atdd/demo/te/stepdefinitons/TransitionalBenefitsSteps.java
+++ src/test/java/com/atdd/demo/te/stepdefinitons/TransitionalBenefitsSteps.java
@@ -1,7 +1,17 @@
 package com.atdd.demo.te.stepdefinitons;
 
 import java.awt.Robot;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
 
+import com.atdd.te.screenHelpers.FunctionalLibrary;
+import com.cucumber.listener.Reporter;
+import com.optumrx.autofusion.core.te.util.Mainframe_GlobalFunctionLib;
+import com.optumrx.autofusion.core.te.util.Screenshot;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import com.atdd.te.screenHelpers.FunctionalLibrary;
 
 public class TransitionalBenefitsSteps {
