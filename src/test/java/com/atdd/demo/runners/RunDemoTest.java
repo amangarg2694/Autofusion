79ac1c5732c2e78e379ee78c89aaf8f09da830a6
fmarcus <fmarcus@lamu02t77a1gtfm.uhc.com>
2018-10-10 12:53:20 -0500

    change path of features file from classpath to src/test/resources/

--- src/test/java/com/atdd/demo/runners/RunDemoTest.java
+++ src/test/java/com/atdd/demo/runners/RunDemoTest.java
@@ -20,7 +20,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(format = { "pretty", "json:target/cucumber.json" },
 
-		features = { "classpath:features/RxClaim/featureFiles/ClaimSub.feature" },
+		features = { "src/test/resources/features/RxClaim/featureFiles/ClaimSub.feature" },
 		plugin = { "rerun:target/rerun.txt", "com.cucumber.listener.ExtentCucumberFormatter:", "html:target/cucumber",
 				"json:target/cucumber.json" },
 
@@ -28,6 +28,10 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 	//	,tags ={"@TC5"}
 		
 )
+
+
+
+
 public class RunDemoTest extends AbstractTestNGCucumberTests { 
 
 	 
